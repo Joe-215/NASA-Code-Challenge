54db666e3015c43bde5d5accb94893e103aaebce
Joseph Kobayashi <joe.cas2511@gmail.com>
2020-06-16 10:13:15 -0400

    Format using prettier

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 class App extends Component {
-
   render() {
     const headerStyle = {
       textShadow: '1px 2px #282794',
-      textAlign: 'center'
-    }
+      textAlign: 'center',
+    };
+
     return (
       <div className="container">
         <div className="card card-body">
-          <h2 style={headerStyle} >NASA's Picture of the Day</h2>
+          <h2 style={headerStyle}>NASA's Picture of the Day</h2>
         </div>
       </div>
-    )
+    );
   }
 }
-export default App
+
+export default App;
--- src/components/DateInput.js
+++ src/components/DateInput.js
@@ -1,25 +1,21 @@
-import React from 'react'
-import DatePicker from 'react-datepicker'
-import 'react-datepicker/dist/react-datepicker.css'
+import React from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 const DateInput = (props) => (
   <div className="text-center">
     <p>
       <em>Pick a Date</em>
     </p>
-    <DatePicker
-      className="form-control"
-      selected={}
-      onChange={}
-    />
+
+    <DatePicker className="form-control" selected={} onChange={} />
+
     <div>
-      <button
-        onClick={}
-        className="btn btn-default"
-      >
+      <button onClick={} className="btn btn-default">
         Random Image
       </button>
     </div>
   </div>
-)
-export default DateInput
+);
+
+export default DateInput;
